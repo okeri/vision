84bb423ed79b82cff6c05dc54b91d75f4e89c1e8
Oleg Keri <ezhi99@gmail.com>
2018-11-11 15:18:09 +0300

    Ported to unstable wayland_protocols

--- src/EGLWindow.cc
+++ src/EGLWindow.cc
@@ -3,69 +3,151 @@
 #include <EGL/egl.h>
 #include <linux/input.h>
 #include <wayland-client.h>
-#include <wayland-egl.h>
 #include <wayland-cursor.h>
+#include <wayland-egl.h>
 
+#include "EGLWindow.hh"
+#include <atomic>
 #include <cstring>
 #include <stdexcept>
-#include "EGLWindow.hh"
+
+#include <xdg-shell-unstable-v6-client-protocol.h>
+#include "xdg-shell-unstable-v6-protocol.c"
+
+#include <iostream>
 
 class EGLWindow::Impl {
-    wl_display *display_;
-    wl_compositor *compositor_;
-    wl_surface *surface_;
-    wl_shell *shell_;
-    wl_shell_surface *shell_surface_;
-    wl_registry *registry_;
-    wl_egl_window *window_;
-    wl_callback *callback_;
-    wl_cursor_theme *cursor_theme_;
-    wl_cursor *default_cursor_;
-    wl_surface *cursor_surface_;
-    wl_seat *seat_;
-    wl_keyboard *keyboard_;
+    wl_display* display_;
+    wl_compositor* compositor_;
+    wl_surface* surface_;
+    wl_registry* registry_;
+    wl_egl_window* window_;
+    wl_cursor_theme* cursor_theme_;
+    wl_cursor* default_cursor_;
+    wl_surface* cursor_surface_;
+    wl_keyboard* keyboard_;
+    wl_seat* seat_;
+
+    zxdg_shell_v6* shell_;
+    zxdg_surface_v6* xdg_surface_;
+    zxdg_toplevel_v6* toplevel_;
+
     EGLSurface egl_surface_;
     EGLDisplay dpy_;
     EGLContext ctx_;
     EGLConfig conf_;
 
     RenderFn render_;
-    bool stop_;
+    std::atomic_bool stop_;
+    bool configured_;
 
-    const wl_callback_listener frame_listener_ = {
-        redraw
-    };
+    bool pressed_;
+    int x_;
+    int y_;
 
   public:
-
     Impl(int width, int height, RenderFn render) :
-            render_(render), stop_(false), keyboard_(NULL) {
+        keyboard_(nullptr),
+        render_(render),
+        stop_(false) {
+        static const wl_keyboard_listener keyboard_listener = {
+            [](void*, wl_keyboard*, uint32_t, int, uint32_t) {},
+            [](void*, wl_keyboard*, uint32_t, wl_surface*, wl_array*) {},
+            [](void*, wl_keyboard*, uint32_t, wl_surface*) {},
+            [](void* data, wl_keyboard*, uint32_t, uint32_t, uint32_t key,
+                uint32_t) {
+                if (key == KEY_ESC || key == KEY_Q) {
+                    reinterpret_cast<Impl*>(data)->stop();
+                }
+            },
+            [](void*, wl_keyboard*, uint32_t, uint32_t, uint32_t, uint32_t,
+                uint32_t) {},
+            nullptr};
+
+        static const wl_seat_listener seat_listener = {
+            [](void* data, wl_seat* seat, unsigned caps) {
+                Impl* window = reinterpret_cast<Impl*>(data);
+
+                if ((caps & WL_SEAT_CAPABILITY_KEYBOARD) &&
+                    !window->keyboard_) {
+                    window->keyboard_ = wl_seat_get_keyboard(seat);
+                    wl_keyboard_add_listener(
+                        window->keyboard_, &keyboard_listener, window);
+                } else if (!(caps & WL_SEAT_CAPABILITY_KEYBOARD) &&
+                           window->keyboard_) {
+                    wl_keyboard_destroy(window->keyboard_);
+                    window->keyboard_ = nullptr;
+                }
+            },
+            nullptr};
+
+        static const zxdg_shell_v6_listener xdg_shell_listener = {
+            [](void*, struct zxdg_shell_v6* shell, uint32_t serial) {
+                zxdg_shell_v6_pong(shell, serial);
+            }};
+
         static const wl_registry_listener registry_listener = {
-            global_registry_handler
-        };
+            [](void* data, wl_registry* registry, uint32_t id,
+                const char* interface, uint32_t) {
+                Impl* window = reinterpret_cast<Impl*>(data);
+                if (strcmp(interface, wl_compositor_interface.name) == 0) {
+                    window->compositor_ =
+                        reinterpret_cast<wl_compositor*>(wl_registry_bind(
+                            registry, id, &wl_compositor_interface, 1));
+                } else if (strcmp(interface, zxdg_shell_v6_interface.name) ==
+                           0) {
+                    window->shell_ =
+                        reinterpret_cast<zxdg_shell_v6*>(wl_registry_bind(
+                            registry, id, &zxdg_shell_v6_interface, 1));
+                    zxdg_shell_v6_add_listener(
+                        window->shell_, &xdg_shell_listener, data);
+
+                } else if (strcmp(interface, wl_seat_interface.name) == 0) {
+                    window->seat_ = reinterpret_cast<wl_seat*>(
+                        wl_registry_bind(registry, id, &wl_seat_interface, 1));
+                    wl_seat_add_listener(window->seat_, &seat_listener, window);
+                } else if (strcmp(interface, wl_shm_interface.name) == 0) {
+                    wl_shm* shm = reinterpret_cast<wl_shm*>(
+                        wl_registry_bind(registry, id, &wl_shm_interface, 1));
+                    window->cursor_theme_ = wl_cursor_theme_load(NULL, 32, shm);
+                    window->default_cursor_ = wl_cursor_theme_get_cursor(
+                        window->cursor_theme_, "left_ptr");
+                }
+            },
+            nullptr};
+
+        static const zxdg_surface_v6_listener xdg_surface_listener = {
+            [](void* data, zxdg_surface_v6* surface, uint32_t serial) {
+                Impl* window = reinterpret_cast<Impl*>(data);
+                zxdg_surface_v6_ack_configure(surface, serial);
+                window->configured_ = true;
+            }};
+
+        static const zxdg_toplevel_v6_listener xdg_toplevel_listener = {
+            [](void*, zxdg_toplevel_v6*, int32_t, int32_t, wl_array*) {},
+            [](void*, zxdg_toplevel_v6*) {}};
 
         display_ = wl_display_connect(NULL);
         if (display_ == nullptr) {
             throw std::runtime_error("Can't connect to display");
         }
+
         registry_ = wl_display_get_registry(display_);
         wl_registry_add_listener(registry_, &registry_listener, this);
         wl_display_dispatch(display_);
+        wl_display_roundtrip(display_);
+
+        if (!compositor_ || !shell_) {
+            throw std::runtime_error("Can't init wayland window");
+        }
 
         static const EGLint context_attribs[] = {
-            EGL_CONTEXT_CLIENT_VERSION, 3,
-            EGL_NONE
-        };
+            EGL_CONTEXT_CLIENT_VERSION, 3, EGL_NONE};
 
-        EGLint config_attribs[] = {
-            EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
-            EGL_RED_SIZE, 1,
-            EGL_GREEN_SIZE, 1,
-            EGL_BLUE_SIZE, 1,
-            EGL_ALPHA_SIZE, 1,
-            EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT,
-            EGL_NONE
-        };
+        EGLint config_attribs[] = {EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+            EGL_RED_SIZE, 1, EGL_GREEN_SIZE, 1, EGL_BLUE_SIZE, 1,
+            EGL_ALPHA_SIZE, 1, EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT,
+            EGL_NONE};
 
         dpy_ = eglGetDisplay(reinterpret_cast<EGLNativeDisplayType>(display_));
         eglInitialize(dpy_, NULL, NULL);
@@ -74,37 +156,35 @@ class EGLWindow::Impl {
         EGLint n;
         eglChooseConfig(dpy_, config_attribs, &conf_, 1, &n);
         ctx_ = eglCreateContext(dpy_, conf_, EGL_NO_CONTEXT, context_attribs);
-        surface_ = wl_compositor_create_surface(compositor_);
-        shell_surface_ = wl_shell_get_shell_surface(shell_, surface_);
 
         surface_ = wl_compositor_create_surface(compositor_);
-        shell_surface_ = wl_shell_get_shell_surface(shell_, surface_);
+        xdg_surface_ = zxdg_shell_v6_get_xdg_surface(shell_, surface_);
+        toplevel_ = zxdg_surface_v6_get_toplevel(xdg_surface_);
+
+        zxdg_surface_v6_add_listener(xdg_surface_, &xdg_surface_listener, this);
+        zxdg_toplevel_v6_add_listener(
+            toplevel_, &xdg_toplevel_listener, nullptr);
+        zxdg_toplevel_v6_set_title(toplevel_, "SecureVision");
+        cursor_surface_ = wl_compositor_create_surface(compositor_);
+        configured_ = false;
 
         window_ = wl_egl_window_create(surface_, width, height);
-        egl_surface_ = eglCreateWindowSurface(
-            dpy_, conf_, reinterpret_cast<EGLNativeWindowType>(window_), NULL);
+        egl_surface_ = eglCreateWindowSurface(dpy_, conf_,
+            reinterpret_cast<EGLNativeWindowType>(window_), nullptr);
+        zxdg_surface_v6_set_window_geometry(xdg_surface_, 0, 0, width, height);
 
-        wl_shell_surface_set_title(shell_surface_, "SecureVision");
-
+        wl_surface_commit(surface_);
+        wl_display_roundtrip(display_);
+        wl_surface_commit(surface_);
         eglMakeCurrent(dpy_, egl_surface_, egl_surface_, ctx_);
-        wl_shell_surface_set_fullscreen(shell_surface_,
-                                        WL_SHELL_SURFACE_FULLSCREEN_METHOD_DEFAULT,
-                                        0, NULL);
-
-        callback_ = wl_display_sync(display_);
-	wl_callback_add_listener(callback_, &frame_listener_, this);
-        cursor_surface_ = wl_compositor_create_surface(compositor_);
     }
 
     ~Impl() {
         wl_egl_window_destroy(window_);
 
-        wl_shell_surface_destroy(shell_surface_);
+        zxdg_toplevel_v6_destroy(toplevel_);
+        zxdg_surface_v6_destroy(xdg_surface_);
         wl_surface_destroy(surface_);
-
-        if (callback_) {
-            wl_callback_destroy(callback_);
-        }
 
         eglMakeCurrent(dpy_, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
         eglTerminate(dpy_);
@@ -113,10 +193,6 @@ class EGLWindow::Impl {
         wl_surface_destroy(cursor_surface_);
         if (cursor_theme_) {
             wl_cursor_theme_destroy(cursor_theme_);
-        }
-
-        if (shell_) {
-            wl_shell_destroy(shell_);
         }
 
         if (compositor_) {
@@ -131,121 +207,25 @@ class EGLWindow::Impl {
     int loop() {
         int ret;
         while (!stop_ && ret != -1) {
-            ret = wl_display_dispatch(display_);
+            if (configured_) {
+                ret = wl_display_dispatch_pending(display_);
+                render_();
+                wl_surface_frame(surface_);
+                eglSwapBuffers(dpy_, egl_surface_);
+            } else {
+                ret = wl_display_dispatch(display_);
+            }
         }
-        return ret;
+        return 0;
     }
 
-    inline void stop() {
+    void stop() {
         stop_ = true;
-    }
-
-    inline void render() {
-        render_();
-    }
-
-    static void global_registry_handler(void *data, wl_registry *registry,
-                                        uint32_t id, const char *interface,
-                                        uint32_t version) {
-        Impl *window = reinterpret_cast<Impl *>(data);
-        if (strcmp(interface, "wl_compositor") == 0) {
-            window->compositor_ =
-                    reinterpret_cast<wl_compositor *>(wl_registry_bind(
-                        registry, id, &wl_compositor_interface, 1));
-        } else if (strcmp(interface, "wl_shell") == 0) {
-            window->shell_ = reinterpret_cast<wl_shell *>(
-                wl_registry_bind(registry, id, &wl_shell_interface, 1));
-        } else if (strcmp(interface, "wl_seat") == 0) {
-            static const struct wl_seat_listener seat_listener = {
-                seat_handle_capabilities,
-            };
-
-            window->seat_ = reinterpret_cast<wl_seat *>(
-                wl_registry_bind(registry, id, &wl_seat_interface, 1));
-            wl_seat_add_listener(window->seat_, &seat_listener, window);
-
-        } else if (strcmp(interface, "wl_shm") == 0) {
-            wl_shm *shm = reinterpret_cast<wl_shm *>(
-                wl_registry_bind(registry, id, &wl_shm_interface, 1));
-            window->cursor_theme_ = wl_cursor_theme_load(NULL, 32, shm);
-            window->default_cursor_ =
-                    wl_cursor_theme_get_cursor(window->cursor_theme_, "left_ptr");
-        }
-
-    }
-    static void seat_handle_capabilities(void *data, struct wl_seat *seat,
-                                         unsigned int caps) {
-        Impl *window = reinterpret_cast<Impl *>(data);
-
-	if ((caps & WL_SEAT_CAPABILITY_KEYBOARD) && !window->keyboard_) {
-            static const struct wl_keyboard_listener keyboard_listener = {
-                keyboard_handle_keymap,
-                keyboard_handle_enter,
-                keyboard_handle_leave,
-                keyboard_handle_key,
-                keyboard_handle_modifiers
-            };
-
-            window->keyboard_ = wl_seat_get_keyboard(seat);
-            wl_keyboard_add_listener(window->keyboard_, &keyboard_listener, window);
-        } else if (!(caps & WL_SEAT_CAPABILITY_KEYBOARD) && window->keyboard_) {
-            wl_keyboard_destroy(window->keyboard_);
-            window->keyboard_ = NULL;
-        }
-    }
-
-    static void keyboard_handle_keymap(void *data, struct wl_keyboard *keyboard,
-                                       uint32_t format, int fd, uint32_t size) {
-    }
-
-    static void  keyboard_handle_enter(void *data, struct wl_keyboard *keyboard,
-                          uint32_t serial, struct wl_surface *surface,
-                          struct wl_array *keys)  {
-    }
-
-    static void keyboard_handle_leave(void *data, struct wl_keyboard *keyboard,
-                                      uint32_t serial, struct wl_surface *surface) {
-    }
-
-    static void keyboard_handle_key(void *data, struct wl_keyboard *keyboard,
-                                    uint32_t serial, uint32_t time, uint32_t key,
-                                    uint32_t state) {
-        if (key == KEY_ESC || key == KEY_Q) {
-            reinterpret_cast<Impl *>(data)->stop();
-        }
-    }
-
-    static void keyboard_handle_modifiers(void *data, struct wl_keyboard *keyboard,
-                                          uint32_t serial, uint32_t mods_depressed,
-                                          uint32_t mods_latched, uint32_t mods_locked,
-                                          uint32_t group) {
-    }
-
-    static void redraw(void *data, wl_callback *callback, uint32_t time) {
-        static bool inited  = false;
-        Impl *window = reinterpret_cast<Impl *>(data);
-        if (callback) {
-            wl_callback_destroy(callback);
-        }
-
-        if (inited) {
-            window->render();
-        } else {
-            inited = true;
-        }
-
-        window->callback_ = wl_surface_frame(window->surface_);
-
-        wl_callback_add_listener(window->callback_,
-                                 &window->frame_listener_,
-                                 window);
-        eglSwapBuffers(window->dpy_, window->egl_surface_);
     }
 };
 
-
 EGLWindow::EGLWindow(int width, int height, RenderFn render) :
-        pImpl_(std::make_unique<EGLWindow::Impl>(width, height, render)) {
+    pImpl_(std::make_unique<EGLWindow::Impl>(width, height, render)) {
 }
 
 EGLWindow::~EGLWindow() {
--- src/common/config.hh
+++ src/common/config.hh
@@ -1,5 +1,5 @@
 #pragma once
 
-#define FAST_POINTS         10
+#define FAST_POINTS          9
 #define FAST_THRESHOLD      30
 #define MEDIAN_KERNEL_SIZE   3
